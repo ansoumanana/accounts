b75fe8178553b67ceab0b6958ec2c9d469f452f7
acisse <ansoumana.cisse@photobox.com>
2022-11-14 09:07:42 +0100

    config server : enable refresh scope

--- src/main/java/com/banksystem/account/AccountApplication.java
+++ src/main/java/com/banksystem/account/AccountApplication.java
@@ -3,12 +3,14 @@ package com.banksystem.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({@ComponentScan("com.banksystem.account.controller")})
 @EnableJpaRepositories("com.banksystem.account.repository")
 @EntityScan("com.banksystem.account.models")
